6f528bbc8587537db9f30077bb2b322d4efb6a43
Joseph Pickering <92154991+pickering-dev@users.noreply.github.com>
2023-02-21 14:56:18 +0000

    burger mobile nav

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -36,27 +36,6 @@ document.addEventListener("DOMContentLoaded", function () {
 		techStackDetails.style.display = "none";
 	});
 
-	// // update progress bar width
-	// function updateProgressBar(event) {
-	// 	const links = Array.from(document.querySelectorAll("nav a"));
-	// 	const index = links.indexOf(event.target);
-	// 	const progressBar = document.querySelector(".bar-progress");
-	// 	const width = index * 20 - 1;
-
-	// 	progressBar.style.width = `${width}%`;
-	// }
-	// // do the same thing but with a scroll event listener
-	// window.addEventListener("scroll", updateProgressBarOnScroll);
-	// function updateProgressBarOnScroll() {
-	// 	const progressBar = document.querySelector(".bar-progress");
-	// 	const scrollPosition = window.scrollY;
-	// 	const totalHeight =
-	// 		document.documentElement.scrollHeight - window.innerHeight;
-	// 	const width = (scrollPosition / totalHeight) * 100;
-	// 	progressBar.style.width = `${width}%`;
-	// 	// console.log(scrollPosition);
-	// }
-
 	const workItems = document.querySelectorAll(".work-item");
 	let activeImage = null;
 
